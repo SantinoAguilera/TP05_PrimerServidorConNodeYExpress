4c88caa9c006fe05d0d8936a5f0c5e122290f7cc
SANTINO AGUILERA <48313116@ort>
2025-04-25 12:05:41 -0300

    Added app.use

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
 })
