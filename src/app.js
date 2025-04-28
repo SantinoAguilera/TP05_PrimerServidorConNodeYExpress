a62acd71b48c3eedf0222de4fdc352c6984d3dfc
Iamatoast <neuyv@protonmail.com>
2025-04-28 08:26:58 -0300

    app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import time from "time.js";
+
+app.get('/', function(req, res) {
+	res.send('WELCOME YOU FOOL');
+});
+
+app.get('/hora', function(req, res) {
+	res.send(time.horaActual());
+});
+
+app.get('/fecha-completa', function(req, res) {
+	res.send(time.fechaActual());
+});
