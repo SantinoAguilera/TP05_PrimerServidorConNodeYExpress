0dce63d942c5897ed2f11ec6e3bb90b4e23eeb7b
MATI EZEQUIEL GRZENDA <48716278@ort>
2025-04-28 09:34:04 -0300

    Falta arreglar algo

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 import express from "express";
 import app from "./src/app.js";
 
-const app = express();
 const port = 3000;
-
-app.use(express.json());
-
-app.use(function (req, res, next) {
-    res.status(404).send("La ruta especificada no existe.");
-})
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
-import time from "time.js";
+import express from "express";
+import time from "./src/time.js";
 
-app.get('/', function(req, res) {
+const app = express();
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
 	res.send('WELCOME YOU FOOL');
 });
 
-app.get('/hora', function(req, res) {
+app.get('/hora', function (req, res) {
 	res.send(time.horaActual());
 });
 
-app.get('/fecha-completa', function(req, res) {
+app.get('/fecha-completa', function (req, res) {
 	res.send(time.fechaActual());
 });
+
+app.use(function (req, res, next) {
+	res.status(404).send("La ruta especificada no existe.");
+})
 
 export default app;
